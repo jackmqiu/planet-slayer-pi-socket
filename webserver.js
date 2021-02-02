49634f3c0ddc2844766807676826260279720a3e
jackmqiu <jackqiu2016@gmail.com>
2021-02-02 16:10:55 +0800

    testing: emits shoot on get/ localhost:8080

--- webserver.js
+++ webserver.js
@@ -1,10 +1,27 @@
 const dotenv = require('dotenv').config();
 
-
+const http = require('http').createServer(handler); //require http server, and create server with function handler()
+const fs = require('fs'); //require filesystem module
 const io = require('socket.io-client');
+
+http.listen(8080); //listen to port 8080
+
 const socket = io.connect(`${process.env.SERVER}3000`, {
     reconnection: true
 })
+
+function handler (req, res) { //create server
+  socket.emit('shoot', 1);
+  fs.readFile(__dirname + '/public/index.html', function(err, data) { //read file index.html in public folder
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
+      return res.end("404 Not Found");
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
+    res.write(data); //write data from index.html
+    return res.end();
+  });
+}
 
 if (process.env.NODE_ENV === 'pi') { //GPIO
   const Gpio = require('onoff').Gpio;
